6c18e7d7ab1a329936de25fdc900c33022b4111f
Spike Vinz <spikevinz@gmail.com>
2022-10-24 09:41:16 +0800

    cleans expo boilerplate

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Blank Canvas</Text>
     </View>
   );
 }
@@ -13,8 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    alignItems: 'center'
+  }
 });
