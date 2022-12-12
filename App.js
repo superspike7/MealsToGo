18168902c4b39dd718eddfff3ce24f2a2428b113
Spike Vinz <spikevinz@gmail.com>
2022-12-12 08:20:41 +0800

    refactors app src architecture

--- App.js
+++ App.js
@@ -1,35 +1,11 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
-import { Searchbar } from "react-native-paper";
+import { RestaurantsScreen } from "./src/features/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.appContainer}>
-        <View style={styles.search}>
-          <Searchbar placeholder="Search"/>
-        </View>
-        <View style={styles.list}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    flexDirection: "column",
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    backgroundColor: "blue",
-    padding: 16,
-    flex: 1,
-  },
-});
